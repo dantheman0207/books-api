7fc0fcd49e568d463feb1ac91a9f45e3eb5c64cd
Daniel Ransom <daniel@danielransom.me>
2016-12-07 14:35:45 +0100

    minor formatting

--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,7 @@ let models = require('../models');
 
 /* Get Book info
  */
-router.get('/user/:user_id/book/:book_id', function(req,res) {
+router.get('/user/:user_id/book/:book_id', function (req, res) {
     let book = req.book;
     book
         .then((book) => {
@@ -14,7 +14,7 @@ router.get('/user/:user_id/book/:book_id', function(req,res) {
 
 /* Get all Books
  */
-router.get('/user/:user_id/book', function(req,res) {
+router.get('/user/:user_id/book', function (req, res) {
     req.user_id
         .then((userID) => {
             let parameters = {
@@ -24,31 +24,31 @@ router.get('/user/:user_id/book', function(req,res) {
             };
             return models.Book.findAll(parameters)
         })
-        .then(function(books) {
+        .then(function (books) {
             res.send(books);
         });
 });
 
 /* Create a Book
-*/
-router.post('/user/:user_id/book', function(req, res) {
-	// validate request
-	let schema = {
-		'name': {
-			notEmpty: true,
-			isLength: {
-              options: [{ min: 2, max: 80 }],
-              errorMessage: 'Must be between 2 and 80 chars long'
-              // Error message for the validator, takes precedent over parameter message
+ */
+router.post('/user/:user_id/book', function (req, res) {
+    // validate request
+    let schema = {
+        'name': {
+            notEmpty: true,
+            isLength: {
+                options: [{min: 2, max: 80}],
+                errorMessage: 'Must be between 2 and 80 chars long'
+                // Error message for the validator, takes precedent over parameter message
             },
-		},
-		'isbn': {
-			optional: true
-		}
-	};
-	req.checkBody(schema);
+        },
+        'isbn': {
+            optional: true
+        }
+    };
+    req.checkBody(schema);
 
-	// create Book
+    // create Book
     req.user_id
         .then((UserId) => {
             let book = {
@@ -65,13 +65,13 @@ router.post('/user/:user_id/book', function(req, res) {
 
 /* Update a Book
  */
-router.put('/user/:user_id/book/:book_id', function(req, res) {
+router.put('/user/:user_id/book/:book_id', function (req, res) {
     // validate request
     let schema = {
         'name': {
             optional: true,
             isLength: {
-                options: [{ min: 2, max: 80 }],
+                options: [{min: 2, max: 80}],
                 errorMessage: 'Must be between 2 and 80 chars long'
                 // Error message for the validator, takes precedent over parameter message
             },
@@ -94,11 +94,11 @@ router.put('/user/:user_id/book/:book_id', function(req, res) {
 
 /* Destroy a Book
  */
-router.delete('/user/:user_id/book/:book_id', function(req, res) {
+router.delete('/user/:user_id/book/:book_id', function (req, res) {
     req.book
         .then((book) => {
             "use strict";
-            let params = {cascade:true};
+            let params = {cascade: true};
             return book.destroy();
         })
         .then(() => {
